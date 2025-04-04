69d32e751576101a40477e911c24a0bdcd4ffd89
Christoph Heidemann <chris_heidemann@outlook.de>
2025-04-05 00:19:35 +0200

    redgion specified

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'eu-central-1_S4LNYngS4', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '7u0h2t3r6g677218h5383blt86', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'eu-central-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
