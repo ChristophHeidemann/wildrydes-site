453d75d8b87f85d805bad1ba9aa1a594bd45eccf
Christoph Heidemann <chris_heidemann@outlook.de>
2025-04-05 00:17:41 +0200

    infos added

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-central-1_S4LNYngS4', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7u0h2t3r6g677218h5383blt86', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'eu-central-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
